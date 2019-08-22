e3a8703dc3dcf03b9888515222376850c5221925
Vinícius Silva <vinicius.ls@live.com>
2019-08-22 16:57:17 -0300

    Implement "summoners" service

--- services/summoners.service.js
+++ services/summoners.service.js
@@ -1,7 +1,19 @@
 const axios = require('axios');
 
 class SummonersService {
+    constructor() {
 
+    }
+
+    async search(username, server) {
+        try {
+            return await axios.get(`${process.env['riotGamesAPIUrl_' + server]}/lol/summoner/v4/summoners/by-name/${username}`);
+        } catch (e) {
+            console.log(`Error requesting summoners data`);
+
+            throw new Error(`Error requesting summoners data. Response: ${e}`);
+        }
+    }
 }
 
 module.exports = { SummonersService };
