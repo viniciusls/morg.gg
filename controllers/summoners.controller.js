0103423aa9442f435d66fd93c52e32910e98cfe9
Vinicius Silva <vinicius.silva@ibm.com>
2019-08-22 15:51:09 -0300

    Implement regex validators to search summoner API

--- controllers/summoners.controller.js
+++ controllers/summoners.controller.js
@@ -1,6 +1,12 @@
-class SummonersController {
-    search() {
+const XRegExp = require('xregexp');
 
+class SummonersController {
+    search(req, res) {
+        const validNameRules = new XRegExp("^[0-9\\p{L} _.]+$");
+
+        if (!XRegExp.test(req.query.username, validNameRules)) {
+            res.status(403).send({ error: 403, msg: `Username ${req.query.username} is not a valid name. Valid names includes any visible Unicode letter characters, digits (0-9), spaces, underscores, and periods.`})
+        }
     }
 }
 
