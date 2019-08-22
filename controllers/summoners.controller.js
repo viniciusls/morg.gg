dead971c19ac91cf1a3265b6ab205362da0d07a7
Vinicius Silva <vinicius.silva@ibm.com>
2019-08-22 17:06:13 -0300

    Implement "summoners" service

--- controllers/summoners.controller.js
+++ controllers/summoners.controller.js
@@ -7,14 +7,14 @@ class SummonersController {
         this.summonersServices = new SummonersService();
     }
 
-    search(req, res) {
+    async search(req, res) {
         const validNameRules = new XRegExp("^[0-9\\p{L} _.]+$");
 
         if (!XRegExp.test(req.query.username, validNameRules)) {
             res.status(403).send({ error: 403, msg: `Username ${req.query.username} is not a valid name. Valid names includes any visible Unicode letter characters, digits (0-9), spaces, underscores, and periods.`})
         }
 
-        this.summonersServices.search(req.query.username, req.query.server)
+        return await this.summonersServices.search(req.query.username, req.query.server)
             .then(data => res.send(data))
             .catch(err => res.status(500).send({ error: 500, message: `Cannot retrieve and process summoners data. Result: ${err}` }))
     }
