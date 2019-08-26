1c1d91f2b7a9d89a60c1d5a6dc72cea2b38dbd65
Vinícius Silva <vinicius.ls@live.com>
2019-08-26 17:01:04 -0300

    Implement validator lib

--- controllers/summoners.controller.js
+++ controllers/summoners.controller.js
@@ -1,6 +1,5 @@
-const XRegExp = require('xregexp');
-
 const { SummonersService } = require('../services/summoners.service');
+const { Validator } = require('../lib/validator');
 
 class SummonersController {
     constructor() {
@@ -8,7 +7,7 @@ class SummonersController {
     }
 
     async search(req, res) {
-        if (!this.validateUsername(req.params.username)) {
+        if (!Validator.validateUsername(req.params.username)) {
             res.status(403).send({ error: 403, msg: `Username ${req.params.username} is not a valid name. Valid names includes any visible Unicode letter characters, digits (0-9), spaces, underscores, and periods.`})
         }
 
@@ -22,7 +21,7 @@ class SummonersController {
     }
 
     async summoner(req, res) {
-        if (!this.validateUsername(req.params.username)) {
+        if (!Validator.validateUsername(req.params.username)) {
             res.status(403).send({ error: 403, msg: `Username ${req.params.username} is not a valid name. Valid names includes any visible Unicode letter characters, digits (0-9), spaces, underscores, and periods.`})
         }
 
@@ -33,12 +32,6 @@ class SummonersController {
 
                 return res.status(500).send({error: 500, message: `Cannot retrieve and process summoner data for ${req.params.username}. ${err}`});
             });
-    }
-
-    validateUsername(username) {
-        const validNameRules = new XRegExp("^[0-9\\p{L} _.]+$");
-
-        return !XRegExp.test(username, validNameRules);
     }
 }
 
